89c98ef95e38101e9851d17b6cf841b3ba98d394
stefanenache12 <stefanenache12@gmail.com>
2023-06-15 17:31:06 +0200

    END PROJECT DADI

--- DADI/js/script.js
+++ DADI/js/script.js
@@ -2,18 +2,34 @@
 // 2) Confronta i due numeri
 // 3) In base al numeri stabilisci un vincitore
 
-const userNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
-const pcNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
 
-console.log('user',userNumber)
-console.log('pc', pcNumber)
 
-if(userNumber > pcNumber){
-    console.log('HAI VINTO')
-}   
-    else if (userNumber == pcNumber){
-    console.log('PAREGGIO')
-    }
-    else {
-    console.log('HAI PERSO')
-    }
+const button = document.getElementById('button')
+
+button.addEventListener('click',
+    function(){
+    const userNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+    const pcNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+
+    document.getElementById('user-number').innerHTML = userNumber;
+    document.getElementById('pc-number').innerHTML = pcNumber;
+
+    const rotate = document.querySelector('.rotate-element');
+    rotate.classList.add('rotate-animation');
+
+    const rotate1 = document.querySelector('.rotate-element1');
+    rotate1.classList.add('rotate-animation');
+    
+    if(userNumber > pcNumber){
+        document.getElementById('messaggio').innerHTML = 'HAI VINTO';
+    }   
+        else if (userNumber == pcNumber){
+            document.getElementById('messaggio').innerHTML = 'PAREGGIO';
+        }
+        else {
+            document.getElementById('messaggio').innerHTML = 'HAI PERSO';
+        }
+}
+    
+)
+
